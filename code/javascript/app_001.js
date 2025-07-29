374c2561b2f2105bbdbace2b22e6dd806bd84f0f
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-29 13:25:57 +0100

    Add code/javascript/app_001.js

---
+++ code/javascript/app_001.js
@@ -0,0 +1,36 @@
+// JavaScript file 1
+const fs = require('fs');
+const path = require('path');
+
+class DataProcessor1 {
+    constructor() {
+        this.data = {};
+        this.processedCount = 0;
+    }
+    
+    async processData(data) {
+        const processed = [];
+        for (const item of data) {
+            if (this.validateItem(item)) {
+                processed.push(this.transformItem(item));
+                this.processedCount++;
+            }
+        }
+        return processed;
+    }
+    
+    validateItem(item) {
+        return item.id && item.name && item.value !== undefined;
+    }
+    
+    transformItem(item) {
+        return {
+            id: item.id,
+            name: item.name.toUpperCase(),
+            value: item.value * 2,
+            processedAt: Date.now()
+        };
+    }
+}
+
+module.exports = DataProcessor1;
